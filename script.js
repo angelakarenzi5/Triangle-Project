d7835275c4fd388e77c3dcaec1a3ed415138b970
Angela Uwase <angelakarenzi5@gmail.com>
2018-12-14 14:17:22 +0200

    edited javascript

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function track() {
   var firstvalue = parseInt(document.getElementById("one").value);
   var secondvalue = parseInt(document.getElementById("two").value);
   var thirdvalue = parseInt(document.getElementById("three").value);
-  var text;
-
-  if (
+  if (firstvalue <= 0 || secondvalue <= 0 || thirdvalue <= 0) {
+    alert("Undefined");
+  } 
+  else if (
     firstvalue === secondvalue &&
     secondvalue === thirdvalue &&
     thirdvalue === firstvalue
   ) {
     alert("Equilateral triangle");
   }
-}
